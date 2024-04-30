12cf26f4d9a29c5105d1d38a7b2ca7292c129d5a
Maya <Mayastridevana0205@gmail.com>
2024-04-30 10:28:14 +0700

    Implementing Videos Feature in Watch View

---
+++ Animalia/Modules/Screen/Watch/VideoListItemView.swift
@@ -0,0 +1,45 @@
+//
+//  VideoListItem.swift
+//  Animalia
+//
+//  Created by User on 30/04/24.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    let video: VideoModel
+    
+    var body: some View {
+        HStack(spacing: 12){
+            ZStack{
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 4)
+            }
+            VStack(alignment: .leading, spacing: 12){
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.accent)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .lineLimit(2)
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    let videos: [VideoModel] = Bundle.main.decode("videos.json")
+    return VideoListItemView(video: videos[4])
+}
